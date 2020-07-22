e0aefa83171126c10ff0cc8ed508941e0236b2e7
jluech <janik@luechinger.ch>
2020-07-22 15:34:32 +0200

    feat: provide utilities for mutation
    refs #109

---
+++ utilities/utils.py
@@ -0,0 +1,65 @@
+import logging
+import os
+from enum import Enum
+from re import match
+
+import yaml
+
+__MUTATOR = None
+__PGA_ID = None
+__PROPERTIES = {}
+
+
+class MessageHandlers(Enum):
+    RabbitMQ = "rabbitMQ",
+
+
+class Mutators(Enum):
+    BitFlip = "bit_flip",
+    Swap = "swap",
+    Scramble = "scramble",
+    Inversion = "inversion",
+
+
+def parse_yaml(yaml_file_path):
+    with open(yaml_file_path, mode="r", encoding="utf-8") as yaml_file:
+        content = yaml.safe_load(yaml_file) or {}
+    return content
+
+
+def get_pga_id():
+    if not __PGA_ID:
+        return __retrieve_pga_id()
+    return __PGA_ID
+
+
+def __retrieve_pga_id():
+    # Retrieve locally saved config file.
+    files = [f for f in os.listdir("/") if match(r'[0-9]+--config\.yml', f)]
+    # https://stackoverflow.com/questions/2225564/get-a-filtered-list-of-files-in-a-directory/2225927#2225927
+    # https://regex101.com/
+
+    if not files.__len__() > 0:
+        raise Exception("Error retrieving the PGA ID: No matching config file found!")
+    config = parse_yaml("/{}".format(files[0]))
+    global __PGA_ID
+    __PGA_ID = config.get("pga_id")
+    logging.info("PGA_ID {id_} retrieved.".format(id_=__PGA_ID))
+    return __PGA_ID
+
+
+def get_property(property_key):
+    return __PROPERTIES[property_key]
+
+
+def set_property(property_key, property_value):
+    __PROPERTIES[property_key] = property_value
+
+
+def forward_mutator():
+    return __MUTATOR
+
+
+def __set_mutator(mutator):
+    global __MUTATOR
+    __MUTATOR = mutator
