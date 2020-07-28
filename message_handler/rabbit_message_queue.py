f7db6899a040629bd895c32cdf04e99fcd91e3f4
jluech <janik@luechinger.ch>
2020-07-28 17:31:50 +0200

    feat: add mock routing to skip fitness evaluation until implemented
    refs #109

--- message_handler/rabbit_message_queue.py
+++ message_handler/rabbit_message_queue.py
@@ -29,7 +29,8 @@ def receive_mutation_callback(channel, method, properties, body):
 
 def send_message_to_queue(channel, payload):
     # Route the message to the next queue in the model.
-    next_recipient = utils.get_messaging_target()
+    # next_recipient = utils.get_messaging_target()
+    next_recipient = "generation"  # TODO: replace with original
     channel.queue_declare(queue=next_recipient, auto_delete=True, durable=True)
 
     # Send message to given recipient.
